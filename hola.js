4fbd71a6bcae5ad0e5ebcfb89ddde42534a9c62f
MateoDelEon670 <109543311+MateoDelEon670@users.noreply.github.com>
2022-08-27 09:53:47 -0700

    Add files via upload

---
+++ hola.js
@@ -0,0 +1,57 @@
+hola=[];
+function fvuyft () {
+var ar=document.getElementById("diez").value;
+var a=document.getElementById("die").value;
+var al=document.getElementById("di").value;
+var ap=document.getElementById("d").value;
+hola.push(ar);
+hola.push(a);
+hola.push(al);
+hola.push(ap);
+console.log(hola);
+
+document.getElementById("hol").innerHTML=hola;
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
